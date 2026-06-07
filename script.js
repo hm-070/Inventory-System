388fbc2caf67136d5e08aeeefd0bf8e4f1bdf8f4
hm07 <harrytmacro@gmail.com>
2026-06-07 14:41:24 +0100

    starting to style

--- script.js
+++ script.js
@@ -3,23 +3,24 @@ const inventory = new Map();
 function addToInventory(form){
     const itemNameFromForm = form.itemName.value;
     const noOfItems = form.amount.value;
-    console.log("item: " + itemNameFromForm + "\n no of items: " + noOfItems);
-    if (isInMap(itemNameFromForm)){
-        const noInList = Number(inventory.get(itemNameFromForm));
-        const newNo = noInList + Number(noOfItems)
-        inventory.set(itemNameFromForm, newNo);
-    } else{
-        inventory.set(itemNameFromForm, noOfItems);
+    if (itemNameFromForm != "" && noOfItems != ""){
+        console.log("item: " + itemNameFromForm + "\n no of items: " + noOfItems);
+        if (isInMap(itemNameFromForm)){
+            const noInList = Number(inventory.get(itemNameFromForm));
+            const newNo = noInList + Number(noOfItems)
+            inventory.set(itemNameFromForm, newNo);
+        } else{
+            inventory.set(itemNameFromForm, noOfItems);
+        }
+        displayMap();
+        updateTable();
     }
-    displayMap();
-    updateTable();
     return false;
 }
 
 window.onload = function(){
     var table = document.getElementById("inventoryTable");
     table.innerHTML = `
-    <br>
     <tr>
         <th>Items</th>
         <th>Amount</th>
@@ -30,7 +31,6 @@ window.onload = function(){
 function updateTable(){
     var table = document.getElementById("inventoryTable");
     table.innerHTML = `
-    <br>
     <tr>
         <th>Items</th>
         <th>Amount</th>
@@ -38,14 +38,14 @@ function updateTable(){
     `;
     for (let [item, amount] of inventory){
         var row = table.insertRow(-1);
-        var cell1 = row.insertCell(0);
-        var cell2 = row.insertCell(1);
-        var cell3 = row.insertCell(2);
-        var cell4 = row.insertCell(3);
-        cell1.innerHTML = item;
-        cell2.innerHTML = amount;
-        cell3.innerHTML = `<input type="button" id="plusOne${item}" value="+" onclick="plusOne('${item}')"><input type="button" id="minusOne${item}" value="-" onclick="minusOne('${item}')">`;
-        cell4.innerHTML = `<input type="button" id="delete${item}" value="Delete" onclick="deleteItem('${item}')">`;
+        var itemCell = row.insertCell(0);
+        var amountCell = row.insertCell(1);
+        var plusMinusCell = row.insertCell(2);
+        var deleteCell = row.insertCell(3);
+        itemCell.innerHTML = item;
+        amountCell.innerHTML = amount;
+        plusMinusCell.innerHTML = `<input type="button" id="plusOne${item}" value="+" onclick="plusOne('${item}')"><input type="button" id="minusOne${item}" value="-" onclick="minusOne('${item}')">`;
+        deleteCell.innerHTML = `<input type="button" id="delete${item}" value="Delete" onclick="deleteItem('${item}')">`;
     }
 }
 
